1ef3ed035bdc96c4431ed3914ea1af5cd2db992c
frnhr <fran@tocka.tk>
2014-10-02 14:54:26 +0200

    Update sync_a_file.py

--- sync_a_file.py
+++ sync_a_file.py
@@ -3,9 +3,17 @@
 from __future__ import print_function
 
 
-FROM_DIR = 'local'
+FROM_DIR = 'rebelmouse'
 TO_DIR = 'remote'
 DELETE_RETRIES = 5
+
+IGNORE_PATTERNS = (
+    r'^.idea',
+    r'^.git/',
+    r'.pyc$',
+    r'___jb_bak___$',
+    r'___jb_old___$',
+)
 
 
 # ------------------------------------------------------------- #
@@ -13,6 +21,7 @@ import sys
 import os
 import shutil
 import re
+import datetime
 
 def _base_dir():
     # return os.getcwd()  # working directory
@@ -24,6 +33,17 @@ def _from_dir(additional_path=''):
 def _to_dir(additional_path=''):
     return os.path.join(_base_dir(), TO_DIR, additional_path)
 
+def _match_ignore(path):
+    for pattern in IGNORE_PATTERNS:
+        if re.search(pattern, path):
+            return True
+    return False
+
+def _print(content, end="\n"):
+    sys.stdout.write("{}".format(content))
+    sys.stdout.write("{}".format(end))
+    sys.stdout.flush()
+
 def _delete(path):
     for i in range(1, DELETE_RETRIES + 1):
         try:
@@ -32,21 +52,24 @@ def _delete(path):
             else:
                 shutil.rmtree(path)
         except OSError as e:
-            print("OS error: {}".format(e))
-            print("retrying {}/{}".format(i, DELETE_RETRIES))
+            _print("OS error: {}".format(e))
+            _print("retrying {}/{}".format(i, DELETE_RETRIES))
         else:
             if i > 1:
                 print("  deleted.")
             break  # for
     if i == DELETE_RETRIES:
-        print("giving up...")
+        _print("giving up...")
 
 def main(args):
     filepath = args[0]
     relative_path = re.sub(r'^{}'.format(_from_dir()), r'', filepath)
+    if _match_ignore(relative_path):
+        return
     from_path = _from_dir(relative_path).strip()
     to_path = _to_dir(relative_path).strip()
-    print(from_path, end=" ")
+    _print(datetime.datetime.now(), end=" ")
+    _print(from_path, end=" ")
     if os.path.isfile(from_path):
         shutil.copy2(from_path, to_path)
         # @TOOD if we replace a file with a directory of exactly the same name, then kaboom!
@@ -57,7 +80,10 @@ def main(args):
     else:
         if os.path.exists(to_path):
             _delete(to_path)
-    print(".")
+    _print(".")
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    try:
+        main(sys.argv[1:])
+    except KeyboardInterrupt:
+        pass
